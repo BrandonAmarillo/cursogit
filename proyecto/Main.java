631ae2a422e05cb23e32b3d0c622f58929705f57
Brandon Amarillo <brandonxdamarillo@gmail.com>
2026-09-22 17:10:43 -0300

    fix: corrección de operación para que resten

--- proyecto/Main.java
+++ proyecto/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public void main(String[] args) {
         System.out.println("Hola mundo");
 
-        int result = 5 + 3;
+        int result = 5 - 3;
         System.out.println("Resultado: " + result);
     }
 }
