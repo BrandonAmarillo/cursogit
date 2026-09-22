62cbc7d21f0b2f2410d8c95cc69accee71865577
Brandon Amarillo <brandonxdamarillo@gmail.com>
2026-09-22 17:09:18 -0300

    feat: agregado suma en main

--- proyecto/Main.java
+++ proyecto/Main.java
@@ -3,5 +3,8 @@ package proyecto;
 public class Main {
     public void main(String[] args) {
         System.out.println("Hola mundo");
+
+        int result = 5 + 3;
+        System.out.println("Resultado: " + result);
     }
 }
