0d1689d7a46effdc30806c1bab91f9db9e446d70
Brandon Amarillo <brandonxdamarillo@gmail.com>
2026-09-23 17:16:05 -0300

    feat: Agregado entidad persona

---
+++ proyecto/Persona.java
@@ -0,0 +1,27 @@
+package proyecto;
+
+public class Persona {
+    private String nombre;
+    private String apellido;
+
+    Persona(String nombre, String apellido) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setApellido(String apellido){
+        this.apellido = apellido;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+}
